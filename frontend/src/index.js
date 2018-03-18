a7c696a100970a03e9934d8606773f93fb64ffa4
Khada <khadakuraki@gmail.com>
2018-03-18 14:24:49 +1100

    Add: Testing setup (jest-cli)

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,7 +7,7 @@ import registerServiceWorker from './registerServiceWorker';
 import 'semantic-ui-css/semantic.min.css';
 // redux
 import { Provider } from 'react-redux';
-import store from './store.js'
+import store from './store'
 // all requests use credentials (for sessions)
 import axios from 'axios';
 axios.defaults.withCredentials = true;
